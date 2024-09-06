40bbb6dc8579dbed0b96aa304cde23c4faaa5666
micuenta33 <mohammedelyousfi@gmial.com>
2024-09-06 15:34:04 +0200

    saving advance

--- src/test/java/com/banana/bananawhatsapp/controladores/ControladorUsuariosTest.java
+++ src/test/java/com/banana/bananawhatsapp/controladores/ControladorUsuariosTest.java
@@ -1,27 +1,37 @@
 package com.banana.bananawhatsapp.controladores;
 
+import com.banana.bananawhatsapp.config.SpringConfig;
 import com.banana.bananawhatsapp.exceptions.UsuarioException;
 import com.banana.bananawhatsapp.modelos.Usuario;
 import com.banana.bananawhatsapp.persistencia.usuarios.IUsuarioRepository;
 import com.banana.bananawhatsapp.util.DBUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {SpringConfig.class})
+@EnableAutoConfiguration
 class ControladorUsuariosTest {
+    @Autowired
     ControladorUsuarios controladorUsuarios;
 
+    @Autowired
     IUsuarioRepository repoUser;
 
 
     @BeforeEach
     void cleanAndReloadData() {
-        DBUtil.reloadDB();
+//        DBUtil.reloadDB();
     }
     @Test
     void dadoUsuarioValido_cuandoAlta_entoncesUsuarioValido() {
@@ -55,10 +65,10 @@ class ControladorUsuariosTest {
     @Test
     void dadoUsuarioNOValido_cuandoActualizar_entoncesExcepcion() {
         assertThrows(UsuarioException.class, () -> {
-            Integer iDUser = 3;
+            Integer iDUser = 2;
             LocalDate fecha = LocalDate.parse("2025-12-17");
             Usuario user = repoUser.obtener(iDUser);
-            user.setNombre("Juan Luis");
+            user.setNombre(null);
             user.setEmail("jl@jll.com");
             user.setAlta(fecha);
             controladorUsuarios.actualizar(user);
